04cc07671ccb7def19c68c816469bbc6f7ca60ec
Rony Xavier <rx294@nyu.edu>
2018-07-13 16:43:56 -0400

    Better reporting  & new sample json
    Signed-off-by: Rony Xavier <rx294@nyu.edu>

--- controls/aws_s3_bucket_objects.rb
+++ controls/aws_s3_bucket_objects.rb
@@ -12,31 +12,35 @@ control "s3-objects-no-public-access" do
 
   tag "fix": "Log into your AWS console and select the S3 buckets section. Select
               the buckets found in your review. For each object in the bucket
-              select the permisssions tab for the object and remove
+              select the permissions tab for the object and remove
               the Public Access permission."
 
+  # keeping track of public objects in this array is the `public_objects` necessary for 
+  # appropriate reporting otherwise, in the case when no public objects are found in the
+  # buckets, the test would end without any reporting.
+  public_objects = []
 
   aws_s3_buckets.bucket_names.each do |bucket|
-
-    public_objects = []
-    
     aws_s3_bucket_objects(bucket).keys.each do |key|
-      public_objects << key if aws_s3_bucket_object(bucket_name: bucket, key: key).public?
-    end
 
+      if aws_s3_bucket_object(bucket_name: bucket, key: key).public?
+          public_objects << key
 
-    public_objects.each do |key|
-      describe aws_s3_bucket_object(bucket_name: bucket, key: key) do
-        it { should_not be_public } 
+        # following code will all the report public objects as fail.
+        describe aws_s3_bucket_object(bucket_name: bucket, key: key) do
+          it { should_not be_public } 
+        end
+
       end
     end
-
-    describe "List of public objects in bucket #{bucket}" do
-      subject { public_objects }
-      it { should be_empty }  
-    end if public_objects.empty?
-
   end
+
+  # following code will report that no public objects were found if `public_objects` array 
+  # is empty which indicates a pass.
+  describe "Number of public objects in S3 Buckets" do
+    subject { public_objects.length }
+    it { should be_zero }  
+  end if public_objects.empty?
 
   describe "Control skipped because no S3 buckets were found" do
     skip "This control is skipped since the aws_s3_buckets resource returned an empty bucket list"
