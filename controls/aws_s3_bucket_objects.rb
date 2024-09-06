a7ae1b7914a1e48135b72a69c9330ec4a4ddb266
Aaron Lippold <lippold@gmail.com>
2024-09-06 09:48:43 -0400

    added error handling, still need to work out cross-region reporting
    Signed-off-by: Aaron Lippold <lippold@gmail.com>

--- controls/aws_s3_bucket_objects.rb
+++ controls/aws_s3_bucket_objects.rb
@@ -1,35 +1,44 @@
-require_relative '../libraries/concurrent_s3'
+require_relative "../libraries/concurrent_s3"
 
-control 'Public_S3_Objects' do
+control "Public_S3_Objects" do
   impact 0.7
-  title 'Ensure there are no publicly accessible S3 objects'
-  desc 'Ensure there are no publicly accessible S3 objects'
-  tag "nist": %w[AC-6]
-  tag "severity": 'high'
+  title "Ensure there are no publicly accessible S3 objects"
+  desc "Ensure there are no publicly accessible S3 objects"
+  tag nist: %w[AC-6]
+  tag severity: "high"
 
-  tag "check": "Review your AWS console and note if any S3 bucket objects are set to
+  tag check:
+        "Review your AWS console and note if any S3 bucket objects are set to
         'Public'. If any objects are listed as 'Public', then this is
         a finding."
 
-  tag "fix": "Log into your AWS console and select the S3 buckets section. Select
+  tag fix:
+        "Log into your AWS console and select the S3 buckets section. Select
         the buckets found in your review. For each object in the bucket
         select the permissions tab for the object and remove
         the Public Access permission."
 
-  exception_bucket_list = input('exception_bucket_list')
+  exception_bucket_list = input("exception_bucket_list")
 
   if aws_s3_buckets.bucket_names.empty?
     impact 0.0
-    desc 'This control is Non Applicable since no S3 buckets were found.'
+    desc "This control is Non Applicable since no S3 buckets were found."
 
-    describe 'This control is Non Applicable since no S3 buckets were found.' do
-      skip 'This control is Non Applicable since no S3 buckets were found.'
+    describe "This control is Non Applicable since no S3 buckets were found." do
+      skip "This control is Non Applicable since no S3 buckets were found."
     end
-  elsif input('single_bucket').present?
-    public_objects = get_public_objects(input('single_bucket').to_s)
-    describe input('single_bucket').to_s do
-      it 'should not have any public objects' do
-        failure_message = public_objects.count > 1 ? "#{public_objects.join(', ')} are public" : "#{public_objects.join(', ')} is public"
+  elsif input("single_bucket").present?
+    public_objects = get_public_objects(input("single_bucket").to_s)
+    describe input("single_bucket").to_s do
+      it "should not have any public objects" do
+        failure_message =
+          (
+            if public_objects.count > 1
+              "#{public_objects.join(", ")} are public"
+            else
+              "#{public_objects.join(", ")} is public"
+            end
+          )
         expect(public_objects).to be_empty, failure_message
       end
     end
@@ -39,8 +48,12 @@ control 'Public_S3_Objects' do
 
       public_objects_multi = get_public_objects(bucket.to_s)
       describe bucket.to_s do
-        it 'should not have any public objects' do
-          failure_message = "#{public_objects_multi.join(', ')} is public"
+        it "should not have any public objects" do
+          failure_message =
+            "\t- #{public_objects_multi.join("\n\t- ")} \n\tis public"
+          failure_message =
+            "\t- #{public_objects_multi.join("\n\t- ")} \n\tare public" if public_objects_multi.count >
+            1
           expect(public_objects_multi).to be_empty, failure_message
         end
       end
--- libraries/concurrent_s3.rb
+++ libraries/concurrent_s3.rb
@@ -1,95 +1,64 @@
-##
-# The above functions utilize multithreading with a thread pool to efficiently process objects in an
-# AWS S3 bucket and identify public objects based on their ACL grants.
-#
-# Args:
-#   myBucket: The code you provided defines methods to retrieve public objects from an AWS S3 bucket
-# using multi-threading for improved performance. The `get_public_objects` method iterates over the
-# objects in the specified S3 bucket, checks their ACL permissions, and collects the keys of objects
-# that are publicly accessible.
-#
-# Returns:
-#   The `get_public_objects` method returns an array of keys for objects in a specified S3 bucket that
-# have public access permissions for all users or authenticated users.
 require "concurrent"
 require "aws-sdk-s3"
 
 module Aws::S3
   class Bucket
     def objects(options = {})
-      options = options.merge(bucket: @name)
-      resp = @client.list_objects_v2(options)
-
-      # Check if the response contains any objects
-      return ObjectSummary::Collection.new([]) if resp.contents.empty?
-
-      pool = Concurrent::FixedThreadPool.new(16)
-      log_thread_pool_status(pool, "Initialized")
-
       batches =
         Enumerator.new do |y|
-          resp.each_page do |page|
-            batch = Concurrent::Array.new
-            page.data.contents.each do |c|
-              begin
+          options = options.merge(bucket: @name)
+          begin
+            resp = @client.list_objects_v2(options)
+            resp.each_page do |page|
+              batch = []
+              pool = Concurrent::FixedThreadPool.new(16)
+              mutex = Mutex.new
+              page.data.contents.each do |c|
                 pool.post do
                   begin
-                    batch << ObjectSummary.new(
-                      bucket_name: @name,
-                      key: c.key,
-                      data: c,
-                      client: @client
-                    )
+                    mutex.synchronize do
+                      batch << ObjectSummary.new(
+                        bucket_name: @name,
+                        key: c.key,
+                        data: c,
+                        client: @client
+                      )
+                    end
                   rescue Aws::S3::Errors::PermanentRedirect => e
-                    # Handle endpoint redirection error
-                    Inspec::Log.debug "Permanent redirect for object #{c.key}: #{e.message}"
+                    Inspec::Log.warn "Permanent redirect for object #{c.key}: #{e.message}"
                   rescue => e
-                    # Handle or log other errors
-                    Inspec::Log.debug "Error processing object #{c.key}: #{e.message}"
-                    Inspec::Log.debug "Backtrace: #{e.backtrace.join("\n")}"
+                    Inspec::Log.warn "Error processing object #{c.key}: #{e.message}"
+                    Inspec::Log.warn "Backtrace: #{e.backtrace.join("\n")}"
                   end
                 end
-              rescue Concurrent::RejectedExecutionError => e
-                # Handle the rejected execution error
-                Inspec::Log.debug "Task submission rejected for object #{c.key}: #{e.message}"
-                log_thread_pool_status(pool, "RejectedExecutionError")
               end
+              pool.shutdown
+              pool.wait_for_termination
+              y.yield(batch)
             end
-            pool.shutdown
-            pool.wait_for_termination
-            y.yield(batch)
+          rescue Aws::S3::Errors::PermanentRedirect => e
+            Inspec::Log.warn "Permanent redirect for bucket #{@name}: #{e.message}"
+          rescue => e
+            Inspec::Log.warn "Error accessing bucket #{@name}: #{e.message}"
+            Inspec::Log.warn "Backtrace: #{e.backtrace.join("\n")}"
           end
         end
       ObjectSummary::Collection.new(batches)
-    ensure
-      pool.shutdown if pool
-    end
-
-    private
-
-    def log_thread_pool_status(pool, context)
-      if Inspec::Log.level == :debug
-        Inspec::Log.debug "Thread pool status (#{context}):"
-        Inspec::Log.debug "  Pool size: #{pool.length}"
-        Inspec::Log.debug "  Queue length: #{pool.queue_length}"
-        Inspec::Log.debug "  Completed tasks: #{pool.completed_task_count}"
-      end
     end
   end
 end
 
 def get_public_objects(myBucket)
-  myPublicKeys = Concurrent::Array.new
+  myPublicKeys = []
   s3 = Aws::S3::Resource.new
   pool = Concurrent::FixedThreadPool.new(56)
-  log_thread_pool_status(pool, "Initialized")
-  debug_mode = Inspec::Log.level == :debug
+  mutex = Mutex.new
 
   begin
     bucket = s3.bucket(myBucket)
     object_count = bucket.objects.count
 
-    if debug_mode
+    if Inspec::Log.level == :debug
       Inspec::Log.debug "### Processing Bucket ### : #{myBucket} with #{object_count} objects"
     end
 
@@ -97,33 +66,24 @@ def get_public_objects(myBucket)
     return myPublicKeys if object_count.zero?
 
     bucket.objects.each do |object|
-      Inspec::Log.debug "    Examining Key: #{object.key}" if debug_mode
-      begin
-        pool.post do
-          begin
-            grants = object.acl.grants
-            if grants.map { |x| x.grantee.type }.any? { |x| x =~ /Group/ } &&
-                 grants
-                   .map { |x| x.grantee.uri }
-                   .any? { |x| x =~ /AllUsers|AuthenticatedUsers/ }
-              myPublicKeys << object.key
-            end
-          rescue Aws::S3::Errors::AccessDenied => e
-            # Handle access denied error
-            Inspec::Log.debug "Access denied for object #{object.key}: #{e.message}"
-          rescue Aws::S3::Errors::PermanentRedirect => e
-            # Handle endpoint redirection error
-            Inspec::Log.debug "Permanent redirect for object #{object.key}: #{e.message}"
-          rescue => e
-            # Handle or log other errors
-            Inspec::Log.debug "Error processing object #{object.key}: #{e.message}"
-            Inspec::Log.debug "Backtrace: #{e.backtrace.join("\n")}"
+      if Inspec::Log.level == :debug
+        Inspec::Log.debug "    Examining Key: #{object.key}"
+      end
+      pool.post do
+        begin
+          grants = object.acl.grants
+          if grants.map { |x| x.grantee.type }.any? { |x| x =~ /Group/ } &&
+               grants
+                 .map { |x| x.grantee.uri }
+                 .any? { |x| x =~ /AllUsers|AuthenticatedUsers/ }
+            mutex.synchronize { myPublicKeys << object.key }
           end
+        rescue Aws::S3::Errors::PermanentRedirect => e
+          Inspec::Log.warn "Permanent redirect for object #{object.key}: #{e.message}"
+        rescue => e
+          Inspec::Log.warn "Error processing object #{object.key}: #{e.message}"
+          Inspec::Log.warn "Backtrace: #{e.backtrace.join("\n")}"
         end
-      rescue Concurrent::RejectedExecutionError => e
-        # Handle the rejected execution error
-        Inspec::Log.debug "Task submission rejected for object #{object.key}: #{e.message}"
-        log_thread_pool_status(pool, "RejectedExecutionError")
       end
     end
 
@@ -131,24 +91,13 @@ def get_public_objects(myBucket)
     pool.shutdown
     pool.wait_for_termination
   rescue Aws::S3::Errors::PermanentRedirect => e
-    # Handle endpoint redirection error for the bucket
-    Inspec::Log.debug "Permanent redirect for bucket #{myBucket}: #{e.message}"
+    Inspec::Log.warn "Permanent redirect for bucket #{myBucket}: #{e.message}"
   rescue => e
-    # Handle or log other errors
-    Inspec::Log.debug "Error accessing bucket #{myBucket}: #{e.message}"
-    Inspec::Log.debug "Backtrace: #{e.backtrace.join("\n")}"
+    Inspec::Log.warn "Error accessing bucket #{myBucket}: #{e.message}"
+    Inspec::Log.warn "Backtrace: #{e.backtrace.join("\n")}"
   ensure
     pool.shutdown if pool
   end
 
   myPublicKeys
-end
-
-def log_thread_pool_status(pool, context)
-  if Inspec::Log.level == :debug
-    Inspec::Log.debug "Thread pool status (#{context}):"
-    Inspec::Log.debug "  Pool size: #{pool.length}"
-    Inspec::Log.debug "  Queue length: #{pool.queue_length}"
-    Inspec::Log.debug "  Completed tasks: #{pool.completed_task_count}"
-  end
 end
